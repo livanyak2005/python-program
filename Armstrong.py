4d4fb9d72095967ef13ae99e40940ab1f2ead4ce
Livanya K <livanyak2005@gmail.com>
2026-06-17 13:20:51 +0530

    Create Armstrong.py

---
+++ Armstrong.py
@@ -0,0 +1,19 @@
+def count(n):
+    c=0
+    while(n):
+        n=n//10
+        c=c+1
+    return c
+n=153
+x=n
+s=0
+c=count(n)
+while(n):
+    a=n%10
+    s=s+a**c
+    n=n//10
+if(x==s):
+        print("Armstrong number")
+else:
+            print("Not else")
+
